7f1abdc24e728810e8607bc49c82c33bec5ca8b8
Kostiantyn Nahula <kostyannagula@gmail.com>
2024-09-10 18:42:50 +0300

    Created base structure of terraform config

---
+++ main.tf
@@ -0,0 +1,159 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_ecs_cluster" "nest_ai_cluster" {
+  name = "nest-ai-cluster"
+}
+
+resource "aws_ecs_task_definition" "nest_ai_task" {
+  family                   = "nest-ai"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "nest-ai",
+      "image": "524147404421.dkr.ecr.eu-west-1.amazonaws.com/nest-ai:a511134-2024-09-09-09-14",
+      "cpu": 256,
+      "memory": 512,
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ]
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTasksExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_default_vpc" "default_vpc" {}
+
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "eu-west-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "eu-west-1b"
+}
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "nest-ai-alb"
+  load_balancer_type = "application"
+  subnets = [
+    aws_default_subnet.default_subnet_a.id,
+    aws_default_subnet.default_subnet_b.id
+  ]
+  security_groups = [aws_security_group.load_balancer_security_group.id]
+}
+
+
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_default_vpc.default_vpc.id # default VPC
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn #  load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn # target group
+  }
+}
+
+resource "aws_ecs_service" "app_service" {
+  name            = "nest-ai-service"                        # Name the service
+  cluster         = aws_ecs_cluster.nest_ai_cluster.id       # Reference the created Cluster
+  task_definition = aws_ecs_task_definition.nest_ai_task.arn # Reference the task that the service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 1 # Set up the number of containers to 3
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn # Reference the target group
+    container_name   = aws_ecs_task_definition.nest_ai_task.family
+    container_port   = 80 # Specify the container port
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    assign_public_ip = true                                                # Provide the containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Set up the security group
+  }
+}
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "app_url" {
+  value = aws_alb.application_load_balancer.dns_name
+}
